5ea867a6c0acf8838579d6d7b99f50b00920ce49
Théo Charré <theocharre2002@gmail.com>
2024-11-03 19:08:21 +0100

    Fix Build

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',  // Dossier de sortie pour les fichiers compilés
+  },
+  // Ajoutez d'autres configurations si nécessaire
+});
